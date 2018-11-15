849c937ea43434eabec193c5563920709678f652
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-11-15 17:34:40 -0200

    Listing array of the photos

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, Dimensions, FlatList } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,14 +18,26 @@ const instructions = Platform.select({
 
 const width = Dimensions.get('screen').width;
 
+const photos = [
+  { id: '1', name: 'Vitor Brangioni' },
+  { id: '2', name: 'Jonny' },
+  { id: '3', name: 'Joaca' },
+];
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-          <Text>Vitor</Text>
-          <Image source={require('./resources/img/mx.jpg')} style={{width, height: width}}/>
-      </View>
+        <FlatList style={{ marginTop: 20 }}
+          data={photos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) =>
+            <View>
+              <Text>{item.name}</Text>
+              <Image source={require('./resources/img/mx.jpg')} style={{ width: width, height: width }} />
+            </View>
+          }
+        />
     );
   }
 }
