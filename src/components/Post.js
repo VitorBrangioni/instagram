e978e4fd40b276ed888bffc6031e3d743a6a1b92
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-11-16 13:04:53 -0200

    Implemented like function and init commentart

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, Dimensions, TouchableOpacity, FlatList, TextInput } from 'react-native';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,23 +25,65 @@ export default class App extends Component<Props> {
     constructor(props) {
         super(props);
         this.state = {
-            photo: this.props.photo
+            photo: { ...this.props.photo, likers: [{}, {}], comentarios: [{ id: 1, login: 'VitorBrangioni', texto: 'Bonita a foto!' }] }
         }
     }
 
     like() {
-        const fotoAtualizada = {
-            ...this.state.photo,
-            likeada: !this.state.photo.likeada
+
+        const { photo } = this.state;
+
+        let likers = [];
+        if (!photo.likeada) {
+            likers = [
+                ...photo.likers,
+                { login: 'VitorBrangioni' }
+            ]
+        } else {
+            likers = photo.likers.filter(like => {
+                if (like.login !== 'VitorBrangioni') {
+                    return like;
+                }
+            })
         }
-        this.setState({photo: fotoAtualizada});
+
+        const fotoAtualizada = {
+            ...photo,
+            likers,
+            likeada: !photo.likeada
+        }
+        this.setState({ photo: fotoAtualizada });
+    }
+
+    showLikes(likers) {
+        if (likers.length == 0)
+            return null;
+
+        return (
+            <Text style={styles.likes}>
+                {likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}
+            </Text>
+        );
+    }
+
+    showLegend(photo) {
+        if (!photo.comentario)
+            return null;
+
+        return (
+            <View style={styles.comentario}>
+                <Text style={styles.tituloComentario}>{photo.loginUsuario}</Text>
+                <Text>{photo.comentario}</Text>
+            </View>
+        )
+
     }
 
     carregaIcone(likeada) {
-        return likeada ? require('../../resources/img/s2-checked.png') : 
+        return likeada ? require('../../resources/img/s2-checked.png') :
             require('../../resources/img/s2.png')
     }
-    
+
     render() {
         const { photo } = this.state;
         return (
@@ -50,12 +92,31 @@ export default class App extends Component<Props> {
                     <Image source={{ uri: photo.urlPerfil }} style={styles.profileImage} />
                     <Text>{photo.loginUsuario}</Text>
                 </View>
-                <Image source={{uri: photo.urlFoto}} style={styles.photo} />
+                <Image source={{ uri: photo.urlFoto }} style={styles.photo} />
                 <View style={styles.footer}>
                     <TouchableOpacity onPress={this.like.bind(this)}>
                         <Image style={styles.likeBtn}
                             source={this.carregaIcone(photo.likeada)} />
                     </TouchableOpacity>
+                    {this.showLikes(photo.likers)}
+                    {this.showLegend(photo)}
+
+                    <FlatList
+                        data={photo.comentarios}
+                        keyExtractor={item => item.id.toString()}
+                        renderItem={({ item }) => 
+                            <View style={styles.comentario}>
+                                <Text style={styles.tituloComentario}>{item.login}</Text>
+                                <Text>{item.texto}</Text>
+                            </View>
+                        }
+                    />
+                    <View style={styles.row}>
+                        <TextInput style={styles.input} placeholder="Adicione um comentário" />
+                        <Image style={styles.btnSend}
+                                source={require('./../../resources/img/send-icon.jpg')} />
+                    </View>
+
                 </View>
             </View>
         );
@@ -78,6 +139,7 @@ const styles = StyleSheet.create({
         height: 40
     },
     photo: {
+        marginBottom: 10,
         width,
         height: width
     },
@@ -85,8 +147,31 @@ const styles = StyleSheet.create({
         height: 40,
         width: 40,
     },
+    likes: {
+        fontWeight: 'bold'
+    },
     footer: {
         margin: 10
+    },
+    comentario: {
+        flexDirection: 'row'
+    },
+    tituloComentario: {
+        fontWeight: 'bold',
+        marginRight: 5
+    },
+    input: {
+        height: 40,
+        flex: 1,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+    },
+    btnSend: {
+        width: 35,
+        height: 35
+    },
+    row: {
+        flexDirection: 'row'
     }
 
 });
